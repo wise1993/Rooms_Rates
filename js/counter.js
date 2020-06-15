13eb18dd0d7ebcac69f65bd28118a199035fa3b4
austinwise1993 <austinwise1993@gmail.com>
2020-06-15 12:07:28 +0100

    First Commit.

---
+++ js/counter.js
@@ -0,0 +1,18 @@
+function numCounter(tagId,maxCount,speed){
+    var initialNumber = 0;
+    function counter(){
+        document.getElementById(tagId).innerHTML = initialNumber;
+        ++initialNumber;
+    }
+    var counterDelay = setInterval(counter,speed);
+    function totalTime(){
+        clearInterval(counterDelay);
+    }
+    var totalPeriod = speed * (maxCount);  
+    setTimeout(totalTime, totalPeriod);
+}
+
+numCounter("Customers",10);
+numCounter("Locations",30);
+numCounter("Rooms",700);
+numCounter("Bookings",100);
